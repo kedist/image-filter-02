31b03ea18e3d905f53bb67a66ec8448ec2625849
kedist <kidytt@yahoo.com>
2022-09-23 20:43:42 +0200

    added a filtered image status

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { Router, Request, Response } from 'express';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
 (async () => {
@@ -28,7 +29,18 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
+app.get('/filterdimage' , async(req: Request, res: Response) => {
+  const image_url = req.query.image_url.toString();
+  if(!image_url){
+    res.status(400).send("image url is required!");
+  }
+  const filterd_image = await filterImageFromURL(image_url);
 
+  res.status(200).sendFile(filterd_image, () => {
+   deleteLocalFiles([filterd_image]); 
+  } )
+
+});
   //! END @TODO1
   
   // Root Endpoint
