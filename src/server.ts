33106ec8ff8392f2f9f137190a9e065ce75ae955
kedist <kidytt@yahoo.com>
2022-09-24 13:31:15 +0200

    update

--- src/server.ts
+++ src/server.ts
@@ -29,7 +29,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
   /**************************************************************************** */
-app.get('/filterdimage' , async(req: Request, res: Response) => {
+app.get('/filteredimage', async(req: Request, res: Response) => {
   const image_url = req.query.image_url.toString();
   if(!image_url){
     res.status(400).send("image url is required!");
